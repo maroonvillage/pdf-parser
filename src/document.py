6dbad48fd9c9d09dfa568e55c50d87cb12f91cc3
Cole P <maroonyear0@gmail.com>
2024-09-30 00:44:46 -0700

    Added to represent a JSON file

---
+++ src/document.py
@@ -0,0 +1,50 @@
+import json
+
+class Figure:
+    def __init__(self, caption):
+        self.caption = caption
+
+class Table:
+    def __init__(self, title):
+        self.title = title
+
+class Section:
+    def __init__(self, heading):
+        self.heading = heading
+        self.paragraphs = []
+        self.figures = []
+        self.tables = []
+
+    def add_paragraph(self, paragraph):
+        self.paragraphs.append(paragraph)
+
+    def add_figure(self, figure):
+        self.figures.append(figure)
+
+    def add_table(self, table):
+        self.tables.append(table)
+
+class Document:
+    def __init__(self, title):
+        self.title = title
+        self.sections = []
+
+    def add_section(self, section):
+        self.sections.append(section)
+
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "sections": [
+                {
+                    "heading": section.heading,
+                    "paragraphs": section.paragraphs,
+                    "figures": [{"caption": fig.caption} for fig in section.figures],
+                    "tables": [{"title": table.title} for table in section.tables]
+                }
+                for section in self.sections
+            ]
+        }
+
+    def to_json(self):
+        return json.dumps(self.to_dict(), indent=4)
