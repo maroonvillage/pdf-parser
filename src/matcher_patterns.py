693a88952dba7fb3eb14368067f2a7270284bcd2
Cole P <maroonyear0@gmail.com>
2024-09-30 00:45:52 -0700

    Added to match section headers that do not have preceding numbers

---
+++ src/matcher_patterns.py
@@ -0,0 +1,43 @@
+from spacy.matcher import Matcher
+import spacy
+
+def get_executive_summary_patterns():
+
+
+    #executive_pattern = [{"LOWER": "executive"}, {"OP": "?"}]
+    #summary_pattern = [{"LOWER": "summary"}, {"OP": "?"}]
+    executive_summary_pattern = [{"LOWER": "executive"}, {"LOWER": "summary"}]
+
+    pattern_list = [executive_summary_pattern]
+
+    return pattern_list
+
+def get_foreward_pattern():
+
+    executive_pattern = [{"LOWER": "foreward"}, {"OP": "?"}]
+    pattern_list = [executive_pattern]
+
+    return pattern_list
+
+def get_introduction_pattern():
+
+    introduction_pattern = [{"LOWER": "introduction"}, {"OP": "?"}]
+    pattern_list = [introduction_pattern]
+
+    return pattern_list
+
+def get_summary_pattern():
+
+    summary_pattern = [{"LOWER": "summary"}, {"OP": "?"}]
+    pattern_list = [summary_pattern]
+
+    return pattern_list
+
+#Not being used
+def get_figure_pattern():
+
+    figure_pattern = [{"LOWER": "figure"}, {"OP": "?"}]
+    fig_pattern = [{"LOWER": "fig."}, {"OP": "?"}]
+    pattern_list = [fig_pattern, figure_pattern]
+
+    return pattern_list
