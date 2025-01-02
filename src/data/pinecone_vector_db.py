94ad6a7c79f455c5e3bcb2859431c9516bdef6f9
Cole P <maroonyear0@gmail.com>
2025-01-01 20:47:11 -0800

    Added helper functions modified logic to create pinecone index without pre-check until correction is made

--- src/data/pinecone_vector_db.py
+++ src/data/pinecone_vector_db.py
@@ -5,6 +5,7 @@ from sentence_transformers import SentenceTransformer
 import logging
 import os
 import json
+import numpy as np
 from typing import List, Dict, Any
 
 class PineConeVectorDB:
@@ -25,11 +26,11 @@ class PineConeVectorDB:
         # Format the date and time as a string (e.g., '2023-04-06_14-30-00')
         timestamp = now.strftime("%Y%m%d%H%M%S")
 
-        self._index_name = f"{prefix.replace('_','').lower()}-{timestamp}"
+        self._index_name = f"{prefix.replace('_','').lower()}{timestamp}"
         self._index = None
         self._pineconedb = Pinecone(api_key=self._api_key)
         self._model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
-        self.log = logging.getLogger(__name__)
+        self._log = logging.getLogger(__name__)
     
     @property
     def api_key(self) -> str:
@@ -40,7 +41,7 @@ class PineConeVectorDB:
     def api_key(self, value: str) -> None:
          """Setter method for API Key"""
          if not value:
-            self.log.error("API Key cannot be empty")
+            self._log.error("API Key cannot be empty")
             raise ValueError("API Key cannot be empty")
          self._api_key = value
 
@@ -54,12 +55,12 @@ class PineConeVectorDB:
     def index(self) -> pinecone.index.Index:
          """Getter method for the Index"""
          if not self._index:
-              self.log.error("Index has not been initialized. Please call `add_embeddings_to_pinecone_index` first.")
+              self._log.error("Index has not been initialized. Please call `add_embeddings_to_pinecone_index` first.")
               raise ValueError("Index has not been initialized. Please call `add_embeddings_to_pinecone_index` first.")
          return self._index
     
-
-    def add_embeddings_to_pinecone_index(self, embeddings: List[List[float]], dim: int = 384, metric: str = 'euclidean',cloud: str = 'aws',region: str = 'us-east-1') -> None:
+    
+    def add_embeddings_to_pinecone_index(self, embeddings: np.ndarray, dim: int = 384, metric: str = 'euclidean',cloud: str = 'aws',region: str = 'us-east-1') -> None:
        """
         Adds embeddings to a Pinecone index.
 
@@ -78,38 +79,34 @@ class PineConeVectorDB:
            raise ValueError("API Key name cannot be empty")
 
        if not self.index_name:
-           self.log.error("Index name cannot be empty")
+           self._log.error("Index name cannot be empty")
            raise ValueError("Index name cannot be empty")
        
        if self._index:
-            self.log.error(f"Index with name {self.index_name} has been initialized. Cannot initialize again.")
+            self._log.error(f"Index with name {self.index_name} has been initialized. Cannot initialize again.")
             raise ValueError(f"Index with name {self.index_name} has been initialized. Cannot initialize again.")
 
        try:
-           if self.index_name not in self._pineconedb.list_indexes().names:
-                self.log.info(f"Creating Pinecone index: {self.index_name}")
-                self._pineconedb.create_index(
-                    name=self.index_name, 
-                    dimension=dim, 
-                    metric=metric,
-                    spec=ServerlessSpec(
-                        cloud=cloud,
-                        region=region
-                    )
-                )
-                self.log.debug(f"Successfully created Pinecone index: {self.index_name}")
-           else:
-                self.log.info(f"Index {self.index_name} already exists, using existing index.")
-            # Connect to the index
-           self._index = self._pineconedb.Index(self.index_name)
-           # Upload section embeddings
-           for i, embedding in enumerate(embeddings):
-                self._index.upsert([(str(i), embedding)])
-           self.log.info(f"Successfully uploaded {len(embeddings)} embeddings to Pinecone index: {self.index_name}")
-
+            pc = Pinecone(api_key=self.api_key)
+            #index_list = pc.list_indexes() #Call the method, and store the result in a variable
+            
+            #self._log.debug(f"Existing indexes: {index_list}")
+            #if self.index_name not in index_list.names: #Access the names attribute using the `index_list` object
+            #if pc.has_index(self.index_name) == False:
+            self._index = _create_pinecone_index(pc, self._log, self.index_name, dim, metric, cloud, region)
+            #if self.index_name not in self._pineconedb.list_indexes().names:
+            #    self._index = _create_pinecone_index(self._pineconedb, self._log, self.index_name, dim, metric, cloud, region)
+            #else:
+            self._log.info(f"Index {self.index_name} already exists, using existing index.")
+                
+                # Connect to the index
+            self._index = self._pineconedb.Index(self.index_name)
+            _upsert_to_pinecone_index(self.index, embeddings)
+            self._log.info(f"Successfully uploaded {len(embeddings)} embeddings to Pinecone index: {self.index_name}")
        except Exception as e:
-            self.log.error(f"An error occurred when creating or upserting to Pinecone Index: {self.index_name}. Error: {e}")
-            raise
+                self._log.error(f"An error occurred when creating or upserting to Pinecone Index: {self.index_name}. Error: {e}")
+                raise
+
 
     def get_vectordb_search_results(self, query: str, top_k: int = 5) -> Dict[str, Any]:
         """
@@ -123,17 +120,18 @@ class PineConeVectorDB:
         """
         
         try:
-            self.log.info(f"Searching Pinecone index: {self.index_name} for query: {query}")
+            self._log.info(f"Searching Pinecone index: {self.index_name} for query: {query}")
             query_embedding = self._model.encode([query]).tolist()
+            self._log.debug(f"Query embedding: {query_embedding}")
             search_results = self.index.query(
-                vector=query_embedding[0],
+                vector=query_embedding,
                 top_k=top_k,
                 include_values=True
             )
-            self.log.debug(f"Search results: {search_results}")
+            self._log.debug(f"Search results: {search_results}")
             return search_results
         except Exception as e:
-           self.log.error(f"An error occurred while searching Pinecone index: {self.index_name} for query: {query}. Error: {e}")
+           self._log.error(f"An error occurred while searching Pinecone index: {self.index_name} for query: {query}. Error: {e}")
            return {}
 
     def output_search_results_to_file(self, prefix: str, keyword: str, search_results: Dict[str, Any], sections: List[str]) -> None:
@@ -166,12 +164,61 @@ class PineConeVectorDB:
 
                 with open(full_path, 'w') as json_file:
                    json.dump(document_json, json_file, indent=2)
-                self.log.info(f"Saved search results to file: {full_path}")
+                self._log.info(f"Saved search results to file: {full_path}")
             else:
-                self.log.warning(f"No search results found for keyword: {keyword}")
+                self._log.warning(f"No search results found for keyword: {keyword}")
         except FileNotFoundError as e:
-             self.log.error(f"FileNotFoundError: {e} when saving file.")
+             self._log.error(f"FileNotFoundError: {e} when saving file.")
         except KeyError as e:
-             self.log.error(f"KeyError: {e} when extracting the data.")
+             self._log.error(f"KeyError: {e} when extracting the data.")
         except Exception as e:
-             self.log.error(f"An unexpected error occurred: {e} when saving the results. Error: {e}")
+             self._log.error(f"An unexpected error occurred: {e} when saving the results. Error: {e}")
+             
+             
+def _create_pinecone_index(db, logger, index_name, dim: int = 384, metric: str = 'euclidean', cloud: str = 'aws', region: str = 'us-east-1') -> pinecone.index.Index:
+     """Creates the pinecone index."""
+     try:
+         logger.info(f"Creating Pinecone index: {index_name}")
+         index =  db.create_index(
+             name=index_name, 
+             dimension=dim, 
+             metric=metric,
+             spec=ServerlessSpec(
+                 cloud=cloud,
+                 region=region
+             )
+         )
+         logger.debug(f"Successfully created Pinecone index: {index_name}")
+         return index
+     except Exception as e:
+         logger.error(f"An error occurred when creating Pinecone Index: {index_name}. Error: {e}")
+         raise
+                  
+def _upsert_to_pinecone_index(index, embeddings: List[List[float]]) -> None:
+    """
+    Upserts embeddings to a Pinecone index.
+
+    Parameters:
+        index (pinecone.index.Index): The Pinecone index.
+        embeddings (List[List[float]]): A list of embeddings (list of list of floats).
+    """
+    try:
+        vectors = []
+        # Iterate over the float list with an index 
+        for i, embedding in enumerate(embeddings): 
+            # Create a dictionary for each float value
+            vector = { "id": str(i), 
+            "values": embedding.tolist()
+            } 
+            # Append the dictionary to the list
+            vectors.append(vector)
+            
+        index.upsert(vectors)
+            
+        # i = 0
+        # for embedding in embeddings:
+        #     index.upsert((str(i), [embedding]))
+        #     i+=1
+
+    except Exception as e:
+        raise Exception(f"An error occurred when upserting to Pinecone Index. Error: {e}")
