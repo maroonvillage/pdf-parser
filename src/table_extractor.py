7b172a3c31a6eddecda1c29a2b86720b6b2d54f6
Cole P <maroonyear0@gmail.com>
2025-01-18 12:07:47 -0800

    refactored two functions to this file

--- src/table_extractor.py
+++ src/table_extractor.py
@@ -1,8 +1,10 @@
 import re
 from typing import List, Dict, Any
+from pdfminer.high_level import extract_pages
 from logger_config import configure_logger
 from processors.element_processors import *
 from document import Table, Row
+from utilities.parse_util import find_table_pattern
 
 def textboxes_to_tabular_json(textboxes: List[Dict[str,Any]], header_footer_dict: List[Dict], y_tolerance: float = 10.0) -> List[Table]:
     """
@@ -41,7 +43,7 @@ def textboxes_to_tabular_json(textboxes: List[Dict[str,Any]], header_footer_dict
                 continue
             
             #Check for table title/caption
-            table_title_match = re.match(r"^(Table\s+\d+[\s\S]*)", textbox_content, re.IGNORECASE)
+            table_title_match = find_table_pattern(textbox_content)
             if(table_title_match):
                 #Check for the string continued ...
                 table_title = table_title_match.group(0).strip()
@@ -113,3 +115,69 @@ def textboxes_to_tabular_json(textboxes: List[Dict[str,Any]], header_footer_dict
     except Exception as e:
          logger.error(f"An unexpected error occurred: {e} when processing list of textboxes.")
          return []
+     
+def get_table_pages_from_unstructured_json(json_data: List[Dict]) -> List[int]:
+    """
+    Extracts the page numbers of tables from the JSON data.
+
+    Parameters:
+        json_data (List[Dict]): The JSON data containing table information.
+
+    Returns:
+        List[int]: A list of page numbers where tables are found.
+    """
+    log = logging.getLogger(__name__)
+    table_data_list = []
+    table_titles = {} #Stores all the titles keyed by element_id
+    current_table = None
+    try:
+      if not json_data:
+            log.warning("The json data is empty.")
+            return []
+      for element in json_data:
+           if element["type"] == "NarrativeText":
+            match = find_table_pattern(element['text'])
+            if match:
+                table_title = match.group(0).strip()
+                title = {
+                    "title": table_title,
+                    "element_id": element["element_id"],
+                    "page_number": element["metadata"].get("page_number", None)
+                }
+                #table_titles[element["metadata"]["parent_id"]] = title
+                table_data_list.append(title)
+                #TEMPORARY
+      
+      return table_data_list
+    except KeyError as e:
+        log.error(f"KeyError: {e} in JSON data")
+        return []
+    except Exception as e:
+        log.error(f"An unexpected error occurred: {e} when processing table pages from the JSON.")
+        return []  
+
+def get_table_pages(pdf_path)-> List[int]:
+    """ Parses a file to find the pages that contain table(s).
+        Returns a list of ints that represent page numbers.
+    """
+    
+    logger = configure_logger(f"{__name__}.get_table_pages")
+    try:
+        table_page_list = []
+        for page_layout in extract_pages(pdf_path):
+            for element in page_layout:
+                if isinstance(element, LTTextBoxHorizontal):
+                    textbox_content = element.get_text()
+                    table_match = find_table_pattern(textbox_content)
+                    if(table_match):
+                        logger.debug("found table match")
+                        title = {
+                            "title": table_match.group(0).strip(),
+                            "page_number": page_layout.pageid
+                        }
+                        table_page_list.append(title)
+    except Exception as e:
+        logger.error(f"An error occurred when parsing the file {pdf_path}. Error: {e}")
+        return []
+    
+    return table_page_list
