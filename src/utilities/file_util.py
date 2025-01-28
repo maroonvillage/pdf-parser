e294a6b649ed5a55a8730b56729281524537fa9c
Cole P <maroonyear0@gmail.com>
2025-01-28 03:39:42 -0800

    Added logic to get_files_from_dir function

--- src/utilities/file_util.py
+++ src/utilities/file_util.py
@@ -160,8 +160,12 @@ def generate_filename(base_name, extension="txt"):
 def get_files_from_dir(directory,extension='.json'):
 
     files =  os.listdir(directory)
-    # Filter the list to include only JSON files 
-    filtered_files = [f for f in files if f.endswith(extension)]
+    if(extension!=None):
+        # Filter the list to include only files with the specified extension
+        filtered_files = [f for f in files if f.endswith(extension)]
+    else:
+         # Filter the list to include files with any extension
+        filtered_files = [f for f in files]
 
     return filtered_files
 
